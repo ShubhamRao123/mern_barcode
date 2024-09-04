935170a72e1fc8debc7c12561e33eed6e56d9c6f
Shubham Rao <shubhamrao834@gmail.com>
2024-09-05 01:52:20 +0530

    edit the app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./component/Navbar";
-import Sidebar from "./component/SIdebar"; // Correct the import spelling for Sidebar
+import Sidebar from "./component/Sidebar"; // Corrected import spelling
 import Receiver from "./pages/Receiver";
 import Sender from "./pages/Sender";
 import Shipment from "./pages/Shipment";
@@ -13,17 +13,27 @@ import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
 
 const App = () => {
+  const location = useLocation(); // Access the current route
+
+  // Check if the current route is for signup or login
+  const isAuthPage =
+    location.pathname === "/" || location.pathname === "/login";
+
   return (
     <ProgressProvider>
       <div className="flex flex-col h-screen bg-gray-900 text-gray-100 overflow-hidden">
         <Navbar />
 
         <div className="flex flex-grow">
-          {/* Sidebar */}
-          <Sidebar />
+          {/* Render Sidebar only if not on signup or login pages */}
+          {!isAuthPage && <Sidebar />}
 
           {/* Main content area */}
-          <div className="flex-grow flex flex-col overflow-y-auto">
+          <div
+            className={`flex-grow flex flex-col overflow-y-auto ${
+              isAuthPage ? "w-full" : ""
+            }`}
+          >
             <Routes>
               <Route path="/" element={<SignUp />} />
               <Route path="/sender" element={<Sender />} />
@@ -36,10 +46,12 @@ const App = () => {
             </Routes>
           </div>
 
-          {/* Rightbar */}
-          <div className="bg-gray-800 w-16 lg:w-56 xl:w-60 h-full">
-            <Rightbar />
-          </div>
+          {/* Render Rightbar only if not on signup or login pages */}
+          {!isAuthPage && (
+            <div className="bg-gray-800 w-16 lg:w-56 xl:w-60 h-full">
+              <Rightbar />
+            </div>
+          )}
         </div>
       </div>
     </ProgressProvider>
